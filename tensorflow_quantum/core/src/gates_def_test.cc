d0d6d79e6317920cacfdbdcfde8058a6d7cb6f46
Antonio Martinez <martantonio@google.com>
2020-02-06 15:25:08 -0800

    Added equality tests for Gate

--- tensorflow_quantum/core/src/gates_def_test.cc
+++ tensorflow_quantum/core/src/gates_def_test.cc
@@ -71,98 +71,75 @@ TEST(GatesDefTest, GateConstructors) {
   }
 }
 
-TEST(CircuitTest, Gate0q) {
-  Gate test_gate, true_gate;
+TEST(GatesDefTest, GateEquality) {
+  // Empty gate
+  Gate test_gate_0q, real_gate_0q;
 
-  // check individual element equality manually
-  ASSERT_EQ(test_gate.time, true_gate.time);
-  ASSERT_EQ(test_gate.num_qubits, true_gate.num_qubits);
+  test_gate_0q.time = real_gate_0q.time + 1;
+  ASSERT_NE(test_gate_0q, real_gate_0q);
+  test_gate_0q.time = real_gate_0q.time;
 
-  // check equality operator overload
-  test_gate.time = true_gate.time + 1;
-  ASSERT_NE(test_gate, true_gate);
-  test_gate.time = true_gate.time;
+  test_gate_0q.num_qubits = real_gate_0q.num_qubits + 1;
+  ASSERT_NE(test_gate_0q, real_gate_0q);
+  test_gate_0q.num_qubits = real_gate_0q.num_qubits;
 
-  test_gate.num_qubits = true_gate.num_qubits + 1;
-  ASSERT_NE(test_gate, true_gate);
-  test_gate.num_qubits = true_gate.num_qubits;
+  ASSERT_EQ(test_gate_0q, real_gate_0q);
 
-  ASSERT_EQ(test_gate, true_gate);
+  // One-qubit gate
+  const unsigned int time1q = 1256;
+  const unsigned int qubits1q = 153;
+  const std::array<float, 8> matrix1q{0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17};
+  Gate test_gate_1q(time1q, qubits1q, matrix1q);
+  Gate real_gate_1q(time1q, qubits1q, matrix1q);
+
+  test_gate_1q.time = real_gate_1q.time + 1;
+  ASSERT_NE(test_gate_1q, real_gate_1q);
+  test_gate_1q.time = real_gate_1q.time;
+
+  test_gate_1q.num_qubits = real_gate_1q.num_qubits + 1;
+  ASSERT_NE(test_gate_1q, real_gate_1q);
+  test_gate_1q.num_qubits = real_gate_1q.num_qubits;
+
+  test_gate_1q.qubits[0] = real_gate_1q.qubits[0] + 1;
+  ASSERT_NE(test_gate_1q, real_gate_1q);
+  test_gate_1q.qubits[0] = real_gate_1q.qubits[0];
+
+  test_gate_1q.matrix[7] = real_gate_1q.matrix[7] + 1;
+  ASSERT_NE(test_gate_1q, real_gate_1q);
+  test_gate_1q.matrix[7] = real_gate_1q.matrix[7];
+
+  ASSERT_EQ(test_gate_1q, real_gate_1q);
+
+  // Two-qubit gate
+  const unsigned int time2q = 2512;
+  const unsigned int qubits2q1 = 253;
+  const unsigned int qubits2q2 = 2256;
+  const std::array<float, 32> matrix2q{0.20, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.210, 0.211, 0.212, 0.213, 0.214, 0.215, 0.216, 0.217, 0.218, 0.219, 0.220, 0.221, 0.223, 0.224, 0.225, 0.226, 0.227, 0.228, 0.229, 0.230, 0.231};
+  Gate test_gate_2q(time2q, qubits2q1, qubits2q2, matrix2q);
+  Gate real_gate_2q(time2q, qubits2q1, qubits2q2, matrix2q);
+
+  test_gate_2q.time = real_gate_2q.time + 1;
+  ASSERT_NE(test_gate_2q, real_gate_2q);
+  test_gate_2q.time = real_gate_2q.time;
+
+  test_gate_2q.num_qubits = real_gate_2q.num_qubits + 1;
+  ASSERT_NE(test_gate_2q, real_gate_2q);
+  test_gate_2q.num_qubits = real_gate_2q.num_qubits;
+
+  test_gate_2q.qubits[0] = real_gate_2q.qubits[0] + 1;
+  ASSERT_NE(test_gate_2q, real_gate_2q);
+  test_gate_2q.qubits[0] = real_gate_2q.qubits[0];
+
+  test_gate_2q.qubits[1] = real_gate_2q.qubits[1] + 1;
+  ASSERT_NE(test_gate_2q, real_gate_2q);
+  test_gate_2q.qubits[1] = real_gate_2q.qubits[1];
+
+  test_gate_2q.matrix[31] = real_gate_2q.matrix[31] + 1;
+  ASSERT_NE(test_gate_2q, real_gate_2q);
+  test_gate_2q.matrix[31] = real_gate_2q.matrix[31];
+
+  ASSERT_EQ(test_gate_2q, real_gate_2q);
 }
-
-TEST(CircuitTest, Gate1q) {
-  Gate test_gate;
-  GetTestGate1q(&test_gate);
-  Gate true_gate;
-  true_gate = test_gate;
-
-  // check individual element equality manually
-  ASSERT_EQ(test_gate.time, true_gate.time);
-  ASSERT_EQ(test_gate.num_qubits, true_gate.num_qubits);
-  ASSERT_EQ(test_gate.qubits[0], true_gate.qubits[0]);
-  for (int i = 0; i < 8; i++) {
-    ASSERT_EQ(test_gate.matrix[i], true_gate.matrix[i]);
-  }
-
-  // check equality operator overload
-  test_gate.time = true_gate.time + 1;
-  ASSERT_NE(test_gate, true_gate);
-  test_gate.time = true_gate.time;
-
-  test_gate.num_qubits = true_gate.num_qubits + 1;
-  ASSERT_NE(test_gate, true_gate);
-  test_gate.num_qubits = true_gate.num_qubits;
-
-  test_gate.qubits[0] = true_gate.qubits[0] + 1;
-  ASSERT_NE(test_gate, true_gate);
-  test_gate.qubits[0] = true_gate.qubits[0];
-
-  test_gate.matrix[7] = true_gate.matrix[7] + 1;
-  ASSERT_NE(test_gate, true_gate);
-  test_gate.matrix[7] = true_gate.matrix[7];
-
-  ASSERT_EQ(test_gate, true_gate);
-}
-
-TEST(CircuitTest, Gate2q) {
-  Gate test_gate;
-  GetTestGate2q(&test_gate);
-  Gate true_gate;
-  true_gate = test_gate;
-
-  // check individual element equality manually
-  ASSERT_EQ(test_gate.time, true_gate.time);
-  ASSERT_EQ(test_gate.num_qubits, true_gate.num_qubits);
-  ASSERT_EQ(test_gate.qubits[0], true_gate.qubits[0]);
-  ASSERT_EQ(test_gate.qubits[1], true_gate.qubits[1]);
-  for (int i = 0; i < 32; i++) {
-    ASSERT_EQ(test_gate.matrix[i], true_gate.matrix[i]);
-  }
-
-  // check equality operator overload
-  test_gate.time = true_gate.time + 1;
-  ASSERT_NE(test_gate, true_gate);
-  test_gate.time = true_gate.time;
-
-  test_gate.num_qubits = true_gate.num_qubits + 1;
-  ASSERT_NE(test_gate, true_gate);
-  test_gate.num_qubits = true_gate.num_qubits;
-
-  test_gate.qubits[0] = true_gate.qubits[0] + 1;
-  ASSERT_NE(test_gate, true_gate);
-  test_gate.qubits[0] = true_gate.qubits[0];
-
-  test_gate.qubits[1] = true_gate.qubits[1] + 1;
-  ASSERT_NE(test_gate, true_gate);
-  test_gate.qubits[1] = true_gate.qubits[1];
-
-  test_gate.matrix[31] = true_gate.matrix[31] + 1;
-  ASSERT_NE(test_gate, true_gate);
-  test_gate.matrix[31] = true_gate.matrix[31];
-
-  ASSERT_EQ(test_gate, true_gate);
-}
-
 
 }  // namespace
 }  // namespace tfq
