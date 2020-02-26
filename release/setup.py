c2474636240615803731a045699c1617447d830b
Michael Broughton <mbbrough@google.com>
2020-02-26 09:28:21 -0800

    added 3.7 tag.

--- release/setup.py
+++ release/setup.py
@@ -70,6 +70,7 @@ setup(
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Scientific/Engineering :: Mathematics',
