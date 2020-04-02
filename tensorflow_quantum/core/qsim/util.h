6aeca5df53f115995a7ccd8c47d20fa596ffa29d
zaqqwerty <antonio.martinez.8128@gmail.com>
2020-04-02 16:21:32 -0400

    brought in parity checker from main branch

--- tensorflow_quantum/core/qsim/util.h
+++ tensorflow_quantum/core/qsim/util.h
@@ -18,6 +18,8 @@ limitations under the License.
 
 #include <cstddef>
 
+#include "absl/container/flat_hash_set.h"
+
 namespace tfq {
 namespace qsim {
 
@@ -26,6 +28,10 @@ void* _aligned_malloc(size_t size);
 
 // Workaround for std::alligned_alloc not working on C++11.
 void _aligned_free(void* ptr);
+
+// Given a set of qubit indices and an integer state sample, return the parity
+// of that set of indices.  Uses the little-endian convention of qsim.
+int ComputeParity(const absl::flat_hash_set<unsigned int>& parity_set, const uint64_t sample);
 
 }  // namespace qsim
 }  // namespace tfq
