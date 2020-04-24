ddf4b18a87ff405729e148117b3f3573842d7be4
Antonio Martinez <martantonio@google.com>
2020-04-24 14:24:24 -0400

    Input check refactor (#220)
    * initial files
    
    * Renamed files
    
    * Updating the tests
    
    * initial compile passing
    
    * input error tests
    
    * finished tests
    
    * updated docstring
    
    * Added rank checks
    
    * Refactored sample layer
    
    * refactored state layer
    
    * build fix, added rank test
    
    * refactored sampled expectation
    
    * removed shape checks
    
    * refactored expectation
    
    * formatting
    
    * removed lint
    
    * Factored out operator expansion
    
    * factored out operator
    
    * Renamed expansion, cut superfluous checks
    
    * update docstrings
    
    * Added tests
    
    * Formatting
    
    * Removed lint
    
    * reverted to original expansion logic
    
    * formatting
    
    * comment

---
+++ tensorflow_quantum/python/layers/circuit_executors/input_checks.py
@@ -0,0 +1,150 @@
+# Copyright 2020 The TensorFlow Quantum Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Input checks common to circuit execution layers."""
+import numpy as np
+import sympy
+import tensorflow as tf
+
+import cirq
+from tensorflow_quantum.python import util
+
+
+def expand_circuits(inputs, symbol_names=None, symbol_values=None):
+    """Function for consistently expanding circuit inputs.
+
+    Args:
+        inputs: a single `cirq.Circuit`, a Python `list` or `tuple` of
+            `cirq.Circuit`s, or a pre-converted `tf.Tensor` of
+            `cirq.Circuit`s.
+        symbol_names: a Python `list` or `tuple` of `str` or `sympy.Symbols`,
+            or a `tf.Tensor` of dtype `string`. These are the symbols
+            parameterizing the input circuits.
+        symbol_values: a Python `list`, `tuple`, or `numpy.ndarray` of
+            floating point values, or `tf.Tensor` of dtype `float32`.
+
+    Returns:
+        inputs: `tf.Tensor` of dtype `string` with shape [batch_size]
+            containing the serialized circuits to be executed.
+        symbol_names: `tf.Tensor` of dtype `string` with shape [n_params], which
+            is used to specify the order in which the values in
+            `symbol_values` should be placed inside of the circuits.
+        symbol_values: `tf.Tensor` of dtype `float32` with shape
+            [batch_size, n_params] specifying parameter values to resolve
+            into the circuits following the ordering dictated by `symbol_names`.
+    """
+    # inputs is the circuit(s).
+    symbols_empty = False
+    if symbol_names is None:
+        symbol_names = []
+    if symbol_values is None:
+        symbols_empty = True
+        symbol_values = [[]]
+
+    # Ingest and promote symbol_names.
+    if isinstance(symbol_names, (list, tuple)):
+        if symbol_names and not all(
+            [isinstance(x, (str, sympy.Symbol)) for x in symbol_names]):
+            raise TypeError("Each element in symbol_names"
+                            " must be a string or sympy.Symbol.")
+        symbol_names = [str(s) for s in symbol_names]
+        if not len(symbol_names) == len(list(set(symbol_names))):
+            raise ValueError("All elements of symbol_names must be unique.")
+        symbol_names = tf.convert_to_tensor(symbol_names,
+                                            dtype=tf.dtypes.string)
+    if not tf.is_tensor(symbol_names):
+        raise TypeError("symbol_names cannot be parsed to string"
+                        " tensor given input: ".format(symbol_names))
+
+    # Ingest and promote symbol_values.
+    if isinstance(symbol_values, (list, tuple, np.ndarray)):
+        symbol_values = tf.convert_to_tensor(symbol_values,
+                                             dtype=tf.dtypes.float32)
+    if not tf.is_tensor(symbol_values):
+        raise TypeError("symbol_values cannot be parsed to float32"
+                        " tensor given input: ".format(symbol_values))
+
+    symbol_batch_dim = tf.gather(tf.shape(symbol_values), 0)
+
+    # Ingest and promote circuit.
+    if isinstance(inputs, cirq.Circuit):
+        # process single circuit.
+        inputs = tf.tile(util.convert_to_tensor([inputs]), [symbol_batch_dim])
+
+    elif isinstance(inputs, (list, tuple, np.ndarray)):
+        # process list of circuits.
+        inputs = util.convert_to_tensor(inputs)
+
+    if not tf.is_tensor(inputs):
+        raise TypeError("circuits cannot be parsed with given input:"
+                        " ".format(inputs))
+
+    if symbols_empty:
+        # No symbol_values were provided. so we must tile up the
+        # symbol values so that symbol_values = [[]] * number of circuits
+        # provided.
+        circuit_batch_dim = tf.gather(tf.shape(inputs), 0)
+        symbol_values = tf.tile(symbol_values, tf.stack([circuit_batch_dim, 1]))
+
+    return inputs, symbol_names, symbol_values
+
+
+def expand_operators(operators=None, circuit_batch_dim=1):
+    """Check and expand operators.
+
+    Args:
+        operators: a single `cirq.PauliString` or `cirq.PauliSum`, a Python
+            `list` or `tuple` of `cirq.PauliString`s or `cirq.PauliSum`s, which
+            will be tiled to have size `circuit_batch_dim` along the first
+            dimension; or a Python `list` or `tuple` (of length
+            `circuit_batch_dim`) of `list`s or `tuple`s of `cirq.PauliString`s
+            or `cirq.PauliSum`s; or pre-converted `tf.Tensor` of
+            `cirq.PauliString`s or `cirq.PauliSum`s.
+        circuit_batch_dim: number of circuits in the final expansion
+
+    Returns:
+        operators: `tf.Tensor` of dtype `string` with shape [batch_size, n_ops]
+            containing the serialized pauli sums to be measured.
+    """
+    if operators is None:
+        raise RuntimeError("Value for operators not provided. operators "
+                           "must be one of cirq.PauliSum, cirq.PauliString"
+                           ", or a list/tensor/tuple containing "
+                           "cirq.PauliSum or cirq.PauliString.")
+
+    op_needs_tile = False
+    if isinstance(operators, (cirq.PauliSum, cirq.PauliString)):
+        # If we are given a single operator promote it to a list and tile
+        # it up to size.
+        operators = [[operators]]
+        op_needs_tile = True
+
+    if isinstance(operators, (list, tuple)):
+        if not isinstance(operators[0], (list, tuple)):
+            # If we are given a flat list of operators. tile them up
+            # to match the batch size of circuits.
+            operators = [operators]
+            op_needs_tile = True
+        operators = util.convert_to_tensor(operators)
+
+    if op_needs_tile:
+        # Don't tile up if the user gave a python list that was precisely
+        # the correct size to match circuits outer batch dim.
+        operators = tf.tile(operators, [circuit_batch_dim, 1])
+
+    if not tf.is_tensor(operators):
+        raise TypeError("operators cannot be parsed to string tensor"
+                        " given input: ".format(operators))
+
+    return operators
---
+++ tensorflow_quantum/python/layers/circuit_executors/input_checks_test.py
@@ -0,0 +1,157 @@
+# Copyright 2020 The TensorFlow Quantum Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for tensorflow_quantum.layers.circuit_executors.input_checks."""
+import numpy as np
+import sympy
+import tensorflow as tf
+
+import cirq
+from tensorflow_quantum.python import util
+from tensorflow_quantum.python.layers.circuit_executors import input_checks
+
+
+class ExpandCircuitsTest(tf.test.TestCase):
+    """Confirm circuits and symbols are upgraded correctly."""
+
+    def test_expand_circuits_error(self):
+        """Test that expand_circuits errors on bad arguments."""
+
+        # Valid test constants
+        qubit = cirq.GridQubit(0, 0)
+        symbol = sympy.Symbol('alpha')
+        names_tensor = tf.convert_to_tensor([str(symbol)],
+                                            dtype=tf.dtypes.string)
+        circuit_tensor = util.convert_to_tensor(
+            [cirq.Circuit(cirq.H(qubit)**symbol)])
+        values_tensor = tf.convert_to_tensor([[0.5]], dtype=tf.dtypes.float32)
+
+        # Bad circuit arg
+        with self.assertRaisesRegex(TypeError,
+                                    expected_regex="circuits cannot be parsed"):
+            input_checks.expand_circuits('junk',
+                                         symbol_names=names_tensor,
+                                         symbol_values=values_tensor)
+
+        # Bad name arg
+        with self.assertRaisesRegex(TypeError,
+                                    expected_regex="string or sympy.Symbol"):
+            input_checks.expand_circuits(circuit_tensor,
+                                         symbol_names=[symbol, 5.0],
+                                         symbol_values=values_tensor)
+        with self.assertRaisesRegex(TypeError,
+                                    expected_regex="string or sympy.Symbol"):
+            input_checks.expand_circuits(circuit_tensor,
+                                         symbol_names=[[]],
+                                         symbol_values=values_tensor)
+        with self.assertRaisesRegex(ValueError,
+                                    expected_regex="must be unique."):
+            input_checks.expand_circuits(circuit_tensor,
+                                         symbol_names=[symbol, symbol],
+                                         symbol_values=values_tensor)
+        with self.assertRaisesRegex(
+                TypeError, expected_regex="symbol_names cannot be parsed"):
+            input_checks.expand_circuits(circuit_tensor,
+                                         symbol_names='junk',
+                                         symbol_values=values_tensor)
+
+        # Bad value arg
+        with self.assertRaisesRegex(
+                TypeError, expected_regex="symbol_values cannot be parsed"):
+            input_checks.expand_circuits(circuit_tensor,
+                                         symbol_names=names_tensor,
+                                         symbol_values='junk')
+
+    def test_allowed_cases(self):
+        """Ensure all allowed input combinations are upgraded correctly."""
+        qubits = cirq.GridQubit.rect(1, 2)
+
+        # Test all symbol-filled circuit configurations
+        names_symbol_list = list(sympy.symbols("s0:2"))
+        names_string_list = [str(s) for s in names_symbol_list]
+        names_symbol_tuple = tuple(names_symbol_list)
+        names_string_tuple = tuple(names_string_list)
+        names_tensor = tf.convert_to_tensor(names_string_list,
+                                            dtype=tf.dtypes.string)
+        circuit_alone = cirq.Circuit(
+            cirq.H(qubits[0])**names_symbol_list[0],
+            cirq.X(qubits[1])**names_symbol_list[1])
+        circuit_list = [circuit_alone for _ in range(3)]
+        circuit_tuple = tuple(circuit_list)
+        circuit_tensor = util.convert_to_tensor(circuit_list)
+        values_list = [[1], [2], [3]]
+        values_tuple = tuple(values_list)
+        values_ndarray = np.array(values_list)
+        values_tensor = tf.convert_to_tensor(values_list,
+                                             dtype=tf.dtypes.float32)
+        for names in [
+                names_symbol_list, names_string_list, names_symbol_tuple,
+                names_string_tuple, names_tensor
+        ]:
+            for circuit in [
+                    circuit_alone, circuit_list, circuit_tuple, circuit_tensor
+            ]:
+                for values in [
+                        values_list, values_tuple, values_ndarray, values_tensor
+                ]:
+                    circuit_test, names_test, values_test = \
+                        input_checks.expand_circuits(circuit, names, values)
+                    self.assertAllEqual(circuit_test, circuit_tensor)
+                    self.assertAllEqual(names_test, names_tensor)
+                    self.assertAllEqual(values_test, values_tensor)
+
+        # Test the case of empty symbols
+        names_tensor = tf.convert_to_tensor([], dtype=tf.dtypes.string)
+        values_tensor = tf.convert_to_tensor([[]] * 3, dtype=tf.dtypes.float32)
+        for circuit in [circuit_list, circuit_tuple, circuit_tensor]:
+            circuit_test, names_test, values_test = \
+                input_checks.expand_circuits(circuit)
+            self.assertAllEqual(circuit_test, circuit_tensor)
+            self.assertAllEqual(names_test, names_tensor)
+            self.assertAllEqual(values_test, values_tensor)
+
+
+class ExpandOperatorsTest(tf.test.TestCase):
+    """Tests of expand_operators."""
+
+    def test_expand_operators_errors(self):
+        """Confirm error on bad input."""
+        with self.assertRaisesRegex(RuntimeError,
+                                    expected_regex="operators not provided"):
+            input_checks.expand_operators()
+        with self.assertRaisesRegex(
+                TypeError, expected_regex="operators cannot be parsed"):
+            input_checks.expand_operators('junk')
+
+    def test_allowed_cases(self):
+        """Confirm that allowed inputs are upgraded correctly."""
+        batch_dim = 3
+        bare_string = cirq.Z(cirq.GridQubit(0, 0))
+        bare_sum = cirq.PauliSum.from_pauli_strings([bare_string])
+        bare_list = [bare_string]
+        bare_tuple = tuple(bare_list)
+        shaped_list = [[bare_string]] * batch_dim
+        shaped_tuple = tuple(shaped_list)
+        op_tensor_single = util.convert_to_tensor([[bare_string]])
+        op_tensor = tf.tile(op_tensor_single, [batch_dim, 1])
+        for op in [
+                bare_string, bare_sum, bare_list, bare_tuple, shaped_list,
+                shaped_tuple, op_tensor
+        ]:
+            op_test = input_checks.expand_operators(op, batch_dim)
+            self.assertAllEqual(op_test, op_tensor)
+
+
+if __name__ == '__main__':
+    tf.test.main()
