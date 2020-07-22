e2f3ddef1c733f0bb58fc629a3cc308083040d5b
zaqqwerty <antonio.martinez.8128@gmail.com>
2020-07-22 16:02:24 -0400

    updating the fie

--- tensorflow_quantum/python/differentiators/sampling_op_parameter_shift.py
+++ tensorflow_quantum/python/differentiators/sampling_op_parameter_shift.py
@@ -29,30 +29,120 @@ def get_differentiable_sample_op_parameter_shift(backend=None,
         backend: Optional Backend to use. Defaults to the native TensorFlow
             Quantum simulator (None), however users may also specify a
             preconfigured cirq execution object to use instead, which must
-            `cirq.Sampler`.
+            inherit `cirq.Sampler`.
         post_process_func: Differentiable function from tensor of
-            type `tf.int8` of shape [repetitions, num_qubits] to a scalar.
-            Assumes same function is to be applied to all sampled outputs.
+            type `tf.int8` of shape [repetitions, num_qubits] to scalar of
+            type `tf.float32`. Given function is applied to all circuits.
     """
     sample_op = circuit_execution_ops.get_sampling_op(backend)
+
+    def sample_post_process(programs, symbol_names, symbol_values, num_samples):
+        ragged_samples = sample_op(programs, symbol_names, symbol_values,
+                                   num_samples)
+        scalar_list = tf.TensorArray(tf.dtypes.float32, tf.shape(programs)[0])
+        for i in tf.range(tf.shape(programs)[0]):
+            scalar_list = scalar_list.write(i, post_process_func(ragged_samples[i]))
+        return scalar_list.stack()
     
     @tf.custom_gradient
     def sample_post_process_wrapper(programs, symbol_names, symbol_values,
-                                    num_samples):        
-        ragged_samples = sample_op(programs, symbol_names, symbol_values,
-                                   num_samples)
-        if post
-        if ragged_samples.shape[0] 
-        for i in tf.range(cpp_ragged.shape[0]):
-            
-            print(cpp_ragged[i].to_tensor())
+                                    num_samples):
+        forward_pass_vals = sample_post_process(programs, symbol_names, symbol_values, num_samples)
         
-        forward_pass_vals = 
         def gradient(grad):
-            self._differentiate_sam(programs, symbol_names,
-                                           symbol_values, pauli_sums,
-                                           num_samples, forward_pass_vals,
-                                           grad)
+            # adapted from `parameter_shift.py`
+            # these get used a lot
+            n_symbols = tf.gather(tf.shape(symbol_names), 0)
+            n_programs = tf.gather(tf.shape(programs), 0)
+            # Assume cirq.decompose() generates gates with at most two distinct
+            # eigenvalues, which results in two parameter shifts.
+            n_shifts = 2
+            
+            # STEP 1: Generate required inputs for executor
+            # Deserialize programs and parse the whole parameterized gates
+            # new_programs has [n_symbols, n_param_gates, n_shifts, n_programs].
+            # These new_programs has programs that parameter-shift rule is applied,
+            # so those programs has
+            (new_programs, weights, shifts,
+             n_param_gates) = parameter_shift_util.parse_programs(
+                 programs, symbol_names, symbol_values, n_symbols)
+            
+            # Reshape & transpose new_programs, weights and shifts to fit into
+            # the input format of tensorflow_quantum simulator.
+            # [n_symbols, n_param_gates, n_shifts, n_programs]
+            new_programs = tf.transpose(new_programs, [0, 2, 3, 1])
+            weights = tf.transpose(weights, [0, 2, 3, 1])
+            shifts = tf.transpose(shifts, [0, 2, 3, 1])
+            
+            # reshape everything to fit into expectation op correctly
+            total_programs = n_programs * n_shifts * n_param_gates * n_symbols
+            # tile up and then reshape to order programs correctly
+            flat_programs = tf.reshape(new_programs, [total_programs])
+            flat_shifts = tf.reshape(shifts, [total_programs])
+            
+            # tile up and then reshape to order ops correctly
+            n_tile = n_shifts * n_param_gates * n_symbols
+            flat_perturbations = tf.concat([
+                tf.reshape(
+                    tf.tile(tf.expand_dims(symbol_values, 0),
+                            tf.stack([n_tile, 1, 1])),
+                    [total_programs, n_symbols]),
+                tf.expand_dims(flat_shifts, axis=1)
+            ],
+                                           axis=1)
+            flat_num_samples = tf.reshape(
+                tf.tile(tf.expand_dims(num_samples, 0), tf.stack([n_tile, 1, 1])),
+                [total_programs, 1])
+            # Append impurity symbol into symbol name
+            new_symbol_names = tf.concat([
+                symbol_names,
+                tf.expand_dims(tf.constant(
+                    parameter_shift_util._PARAMETER_IMPURITY_NAME),
+                               axis=0)
+            ],
+                                         axis=0)
+            
+            # STEP 2: calculate the required expectation values
+            expectations = sample_post_process(flat_programs, new_symbol_names,
+                                               flat_perturbations,
+                                               flat_num_samples)
+            
+            # STEP 3: generate gradients according to the results
+            
+            # we know the rows are grouped according to which parameter
+            # was perturbed, so reshape to reflect that
+            grouped_expectations = tf.reshape(
+                expectations,
+                [n_symbols, n_shifts * n_programs * n_param_gates, -1])
+            
+            # now we can calculate the partial of the circuit output with
+            # respect to each perturbed parameter
+            def rearrange_expectations(grouped):
+                
+                def split_vertically(i):
+                    return tf.slice(grouped, [i * n_programs, 0],
+                                    [n_programs, 1])
+                
+                return tf.map_fn(split_vertically,
+                                 tf.range(n_param_gates * n_shifts),
+                                 dtype=tf.float32)
+            
+            # reshape so that expectations calculated on different programs are
+            # separated by a dimension
+            rearranged_expectations = tf.map_fn(rearrange_expectations,
+                                                grouped_expectations)
+            
+            # now we will calculate all of the partial derivatives
+            partials = tf.einsum(
+                'spco,spc->sco', rearranged_expectations,
+                tf.cast(
+                    tf.reshape(weights,
+                               [n_symbols, n_param_gates * n_shifts, n_programs]),
+                    rearranged_expectations.dtype))
+            
+            # now apply the chain rule
+            this_grad_vec = tf.einsum('sco,co -> cs', partials, grad)
+
             return None, None, this_grad_vec, None
 
         return forward_pass_vals, gradient
