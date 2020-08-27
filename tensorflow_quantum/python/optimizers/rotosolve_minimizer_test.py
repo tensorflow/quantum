c0ab8e811d4916ac74fd4b5fd05e1c7850ceabb9
Shuxiang Cao <shuxiang.cao@rahko.ai>
2020-08-27 05:31:01 +0000

    Update test

--- tensorflow_quantum/python/optimizers/rotosolve_minimizer_test.py
+++ tensorflow_quantum/python/optimizers/rotosolve_minimizer_test.py
@@ -87,7 +87,7 @@ class RotosolveMinimizerTest(tf.test.TestCase, parameterized.TestCase):
         coefficient = tf.random.uniform(shape=[n])
         min_value = -tf.sum(tf.abs(coefficient))
 
-        func = lambda x:tf.sum(tf.sin(x) * coefficient)
+        func = lambda x: tf.sum(tf.sin(x) * coefficient)
 
         result = rotosolve_minimizer.minimize(func, np.random.random(n))
 
