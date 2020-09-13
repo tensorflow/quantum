fef587c97b738734abec336d741a29510f6ef258
Coxious <coxious@coxious.com>
2020-09-13 12:08:23 +0800

    Fix lint

--- tensorflow_quantum/python/optimizers/rotosolve_minimizer.py
+++ tensorflow_quantum/python/optimizers/rotosolve_minimizer.py
@@ -16,8 +16,8 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-import numpy as np
 import collections
+import numpy as np
 import tensorflow as tf
 
 
