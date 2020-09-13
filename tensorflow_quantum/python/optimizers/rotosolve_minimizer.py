baf5e998405aa2f219c476595655b7edc21319e3
Coxious <coxious@coxious.com>
2020-09-13 12:09:50 +0800

    Remove unused input

--- tensorflow_quantum/python/optimizers/rotosolve_minimizer.py
+++ tensorflow_quantum/python/optimizers/rotosolve_minimizer.py
@@ -13,9 +13,6 @@
 # limitations under the License.
 # ==============================================================================
 """The rotosolve minimization algorithm"""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 import collections
 import numpy as np
 import tensorflow as tf
