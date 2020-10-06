f9a015a5664dce88ca99f6de85eaf5376ae0cf3a
Antonio Martinez <martantonio@google.com>
2020-10-06 15:10:30 -0400

    Updated install instructions for new TF and Bazel versions (#343)
    * Updated install instructions for new TF and Bazel versions
    
    * updated pip instruction too
    
    * updated tutorial installs to tf 2.3.0
    
    * updated pip install docstring
    
    * update to 2.3.1
    
    Co-authored-by: MichaelBroughton <MichaelBroughton@users.noreply.github.com>

--- scripts/build_pip_package_test.sh
+++ scripts/build_pip_package_test.sh
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
 pip install -r requirements.txt
 
 # cd tensorflow_quantum
