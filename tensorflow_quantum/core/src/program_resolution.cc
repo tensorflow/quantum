7d1a93ef95386d63ab5f5eea816c4cc347c38f99
Jae Yoo <jaeyoo@google.com>
2021-03-24 22:56:20 +0900

    Fix format cc

--- tensorflow_quantum/core/src/program_resolution.cc
+++ tensorflow_quantum/core/src/program_resolution.cc
@@ -121,7 +121,8 @@ Status ResolveQubitIds(Program* program, unsigned int* num_qubits,
       // Resolve control qubit ids found in the control_qubits arg.
       absl::string_view control_qubits =
           operation.args().at("control_qubits").arg_value().string_value();
-      if (control_qubits.empty()) {  // explicit empty value set in serializer.py.
+      // explicit empty value set in serializer.py.
+      if (control_qubits.empty()) {
         continue;
       }
       std::vector<absl::string_view> control_ids =
@@ -208,7 +209,8 @@ Status ResolveQubitIds(Program* program, unsigned int* num_qubits,
       // Resolve control qubit ids found in the control_qubits arg.
       absl::string_view control_qubits =
           operation.args().at("control_qubits").arg_value().string_value();
-      if (control_qubits.empty()) {  // explicit empty value set in serializer.py.
+      // explicit empty value set in serializer.py.
+      if (control_qubits.empty()) {
         continue;
       }
       std::vector<absl::string_view> control_ids =
