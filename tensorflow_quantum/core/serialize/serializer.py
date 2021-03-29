f09dfe818d4354f990c1d9e3bdb1d9d4077202e4
Michael Broughton <mbbrough@google.com>
2021-03-29 14:31:11 -0700

    Added PD channel support.

--- tensorflow_quantum/core/serialize/serializer.py
+++ tensorflow_quantum/core/serialize/serializer.py
@@ -231,6 +231,40 @@ def _depolarize_channel_deserializer():
         args=args)
 
 
+def _phase_damp_channel_serializer():
+    """Make standard serializer for PhaseDamp channel."""
+
+    args = [
+        # cirq channels can't contain symbols.
+        cirq.google.SerializingArg(serialized_name="gamma",
+                                   serialized_type=float,
+                                   op_getter=lambda x: x.gate.gamma),
+        cirq.google.SerializingArg(serialized_name="control_qubits",
+                                   serialized_type=str,
+                                   op_getter=lambda x: ''),
+        cirq.google.SerializingArg(serialized_name="control_values",
+                                   serialized_type=str,
+                                   op_getter=lambda x: '')
+    ]
+    return cirq.google.GateOpSerializer(gate_type=cirq.PhaseDampingChannel,
+                                        serialized_gate_id="PD",
+                                        args=args,
+                                        can_serialize_predicate=_CONSTANT_TRUE)
+
+
+def _phase_damp_channel_deserializer():
+    """Make standard deserializer for PhaseDamp channel."""
+
+    args = [
+        cirq.google.DeserializingArg(serialized_name="gamma",
+                                     constructor_arg_name="gamma")
+    ]
+    return cirq.google.GateOpDeserializer(
+        serialized_gate_id="PD",
+        gate_constructor=cirq.PhaseDampingChannel,
+        args=args)
+
+
 def _eigen_gate_serializer(gate_type, serialized_id):
     """Make standard serializer for eigen gates."""
 
@@ -532,7 +566,8 @@ SERIALIZERS = [
     _asymmetric_depolarize_serializer(),
     _depolarize_channel_serializer(),
     _fsim_gate_serializer(),
-    _identity_gate_serializer()
+    _identity_gate_serializer(),
+    _phase_damp_channel_serializer(),
 ]
 
 DESERIALIZERS = [
@@ -545,7 +580,8 @@ DESERIALIZERS = [
     _asymmetric_depolarize_deserializer(),
     _depolarize_channel_deserializer(),
     _fsim_gate_deserializer(),
-    _identity_gate_deserializer()
+    _identity_gate_deserializer(),
+    _phase_damp_channel_deserializer(),
 ]
 
 SERIALIZER = cirq.google.SerializableGateSet(gate_set_name="tfq_gate_set",
--- tensorflow_quantum/core/serialize/serializer_test.py
+++ tensorflow_quantum/core/serialize/serializer_test.py
@@ -413,7 +413,11 @@ def _get_noise_proto_pairs():
         (cirq.Circuit(
             cirq.asymmetric_depolarize(p_x=0.1, p_y=0.2, p_z=0.3)(q0)),
          _build_op_proto("ADP", ['p_x', 'p_y', 'p_z'], [0.1, 0.2, 0.3],
-                         ['0_0']))
+                         ['0_0'])),
+
+        # Phase damp.
+        (cirq.Circuit(cirq.phase_damp(gamma=0.1)(q0)),
+         _build_op_proto("PD", ['gamma'], [0.1], ['0_0']))
     ]
     return pairs
 
