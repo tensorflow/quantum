2a1259494361ef64f34912924ff891512aaa299e
Jae Yoo <jaeyoo@google.com>
2021-04-03 09:32:31 +0900

    Fix format

--- tensorflow_quantum/core/ops/math_ops/__init__.py
+++ tensorflow_quantum/core/ops/math_ops/__init__.py
@@ -15,4 +15,4 @@
 """Module for tfq.core.ops.math_ops.*"""
 
 from tensorflow_quantum.core.ops.math_ops.inner_product_op import (
-  inner_product, inner_product_hessian)
+    inner_product, inner_product_hessian)
