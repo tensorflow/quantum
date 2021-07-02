deecd9fa82c852450bc6e11a7c503d1c909703a6
Tony Bruguier <tony.bruguier@gmail.com>
2021-07-02 05:43:45 +0000

    Unit test with one sine wave

--- quantum_embed/quantum_embed_test.py
+++ quantum_embed/quantum_embed_test.py
@@ -14,6 +14,7 @@
 # ==============================================================================
 
 import cirq
+import math
 import numpy as np
 
 import tensorflow as tf
@@ -25,15 +26,8 @@ import quantum_embed
 class QuantumEmbedTest(tf.test.TestCase):
     """Tests for the QuantumEmbed layer."""
 
-    def test_pqc_double_learn(self):
-        """Test a simple learning scenario using analytic and sample expectation
-        on many backends."""
-        num_repetitions_input = 2
-        depth_input = 5
-        num_unitary_layers = 7
-        num_repetitions = 11
-        num_examples = 13
-
+    def _train(self, num_repetitions_input, depth_input, num_unitary_layers,
+               num_repetitions, num_examples, data_in, data_out, epochs):
         qubits = [[cirq.GridQubit(i, j)
                    for j in range(depth_input)]
                   for i in range(num_repetitions_input)]
@@ -48,21 +42,64 @@ class QuantumEmbedTest(tf.test.TestCase):
         model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.1),
                       loss=tf.keras.losses.mean_squared_error)
 
-        data_in = np.random.normal(0.0, 0.1, (
-            num_examples,
-            depth_input,
-        ))
         data_circuits = util.convert_to_tensor([
             cirq.Circuit(qe.build_param_rotator(data_in[m, :]))
             for m in range(num_examples)
         ])
+
+        return model.fit(x=data_circuits, y=data_out, epochs=epochs)
+
+    def test_dimensions(self):
+        """Test that dimensions are handled properly by using prime numbers."""
+        num_repetitions_input = 2
+        depth_input = 5
+        num_unitary_layers = 7
+        num_repetitions = 11
+        num_examples = 13
+
+        data_in = np.random.normal(0.0, 0.1, (
+            num_examples,
+            depth_input,
+        ))
         data_out = np.array(
             [[2.0 * (np.linalg.norm(data_in[m, :]) < 0.15) - 1.0]
              for m in range(num_examples)],
             dtype=np.float32)
 
-        history = model.fit(x=data_circuits, y=data_out, epochs=20)
+        history = self._train(num_repetitions_input,
+                              depth_input,
+                              num_unitary_layers,
+                              num_repetitions,
+                              num_examples,
+                              data_in,
+                              data_out,
+                              epochs=20)
+
         assert history.history['loss'][-1] < history.history['loss'][0] * 0.5
+
+    def test_1_sine_wave(self):
+        num_repetitions_input = 1
+        depth_input = 1
+        num_unitary_layers = 1
+        num_repetitions = 1
+        num_examples = 100
+
+        data_in = np.repeat(np.expand_dims(np.linspace(0, 2.0 * np.pi,
+                                                       num_examples),
+                                           axis=1),
+                            depth_input,
+                            axis=1)
+        data_out = np.cos(data_in - 0.20160913)
+
+        history = self._train(num_repetitions_input,
+                              depth_input,
+                              num_unitary_layers,
+                              num_repetitions,
+                              num_examples,
+                              data_in,
+                              data_out,
+                              epochs=100)
+        assert history.history['loss'][-1] < 1e-3
 
 
 if __name__ == "__main__":
