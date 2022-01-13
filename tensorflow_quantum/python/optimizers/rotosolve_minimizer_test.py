73f1e4f1f7ceddd5d7b51d54977cd43ed772eb35
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2022-01-13 15:04:34 -0700

    Update rotosolve_minimizer_test.py

--- tensorflow_quantum/python/optimizers/rotosolve_minimizer_test.py
+++ tensorflow_quantum/python/optimizers/rotosolve_minimizer_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Test module for tfq.python.optimizers.rotosolve_minimizer optimizer."""
+"""Test module for tfq.python.optimizers.spsa_minimizer optimizer."""
 # Remove PYTHONPATH collisions for protobuf.
 # pylint: disable=wrong-import-position
 import sys
@@ -29,7 +29,7 @@ import cirq
 import sympy
 from tensorflow_quantum.python.layers.high_level import pqc
 from tensorflow_quantum.python import util
-from tensorflow_quantum.python.optimizers import rotosolve_minimizer
+from tensorflow_quantum.python.optimizers import spsa_minimizer
 
 
 def loss_function_with_model_parameters(model, loss, train_x, train_y):
@@ -61,7 +61,7 @@ def loss_function_with_model_parameters(model, loss, train_x, train_y):
     # Function accept the parameter and evaluate model
     @tf.function
     def func(params):
-        """A function that can be used by tfq.optimizer.rotosolve_minimize.
+        """A function that can be used by tfq.optimizer.spsa_minimize.
 
         Args:
            params [in]: a 1D tf.Tensor.
@@ -79,44 +79,141 @@ def loss_function_with_model_parameters(model, loss, train_x, train_y):
 
         # evaluate the loss
         loss_value = loss(model(train_x, training=True), train_y)
+        if loss_value.shape != ():
+            loss_value = tf.cast(tf.math.reduce_mean(loss_value), tf.float32)
         return loss_value
 
     return func
 
 
-class RotosolveMinimizerTest(tf.test.TestCase, parameterized.TestCase):
-    """Tests for the rotosolve optimization algorithm."""
-
-    def test_function_optimization(self):
-        """Optimize a simple sinusoid function."""
-
-        n = 10  # Number of parameters to be optimized
-        coefficient = tf.random.uniform(shape=[n])
-        min_value = -tf.math.reduce_sum(tf.abs(coefficient))
-
-        func = lambda x: tf.math.reduce_sum(tf.sin(x) * coefficient)
-
-        result = rotosolve_minimizer.minimize(func, np.random.random(n))
-
-        self.assertAlmostEqual(func(result.position), min_value)
-        self.assertAlmostEqual(result.objective_value, min_value)
-        self.assertTrue(result.converged)
-        self.assertLess(result.num_iterations,
-                        50)  # 50 is the default max iteration
+class SPSAMinimizerTest(tf.test.TestCase, parameterized.TestCase):
+    """Tests for the SPSA optimization algorithm."""
 
     def test_nonlinear_function_optimization(self):
         """Test to optimize a non-linear function.
-        A non-linear function cannot be optimized by rotosolve,
-        therefore the optimization must never converge.
         """
         func = lambda x: x[0]**2 + x[1]**2
 
-        result = rotosolve_minimizer.minimize(func,
-                                              tf.random.uniform(shape=[2]))
+        result = spsa_minimizer.minimize(func, tf.random.uniform(shape=[2]))
+        self.assertAlmostEqual(func(result.position).numpy(), 0, delta=1e-4)
+        self.assertTrue(result.converged)
 
+    def test_quadratic_function_optimization(self):
+        """Test to optimize a sum of quadratic function.
+        """
+        n = 2
+        coefficient = tf.random.uniform(minval=0, maxval=1, shape=[n])
+        func = lambda x: tf.math.reduce_sum(np.power(x, 2) * coefficient)
+
+        result = spsa_minimizer.minimize(func, tf.random.uniform(shape=[n]))
+        self.assertAlmostEqual(func(result.position).numpy(), 0, delta=2e-4)
+        self.assertTrue(result.converged)
+
+    def test_noisy_sin_function_optimization(self):
+        """Test noisy ssinusoidal function
+        """
+        n = 10
+        func = lambda x: tf.math.reduce_sum(
+            tf.math.sin(x) + tf.random.uniform(
+                minval=-0.1, maxval=0.1, shape=[n]))
+
+        result = spsa_minimizer.minimize(func, tf.random.uniform(shape=[n]))
+        self.assertLessEqual(func(result.position).numpy(), -n + 0.1 * n)
+
+    def test_failure_optimization(self):
+        """Test a function that is completely random and cannot be minimized
+        """
+        n = 100
+        func = lambda x: np.random.uniform(-10, 10, 1)[0]
+        it = 50
+
+        result = spsa_minimizer.minimize(func,
+                                         tf.random.uniform(shape=[n]),
+                                         max_iterations=it)
         self.assertFalse(result.converged)
-        self.assertEqual(result.num_iterations,
-                         50)  # 50 is the default max iteration
+        self.assertEqual(result.num_iterations, it)
+
+    def test_blocking(self):
+        """Test the blocking functionality.
+        """
+        n = 10
+        it = 50
+
+        init = 1
+        self.incr = 0
+        def block_func1(params):
+            self.incr += init
+            return self.incr
+
+        result = spsa_minimizer.minimize(block_func1,
+                                         tf.random.uniform(shape=[n]),
+                                         blocking=True,
+                                         allowed_increase=0.5,
+                                         max_iterations=it)
+        self.assertFalse(result.converged)
+        self.assertEqual(result.num_iterations, it)
+        self.assertEqual(result.objective_value,
+                         init * 4)  # function executd 3 (in step) +
+        # 1 (initial evaluation) times
+
+        init = 1 / 6 * 0.49
+        self.incr = 0
+        def block_func2(params):
+            self.incr += init
+            return self.incr
+
+        result = spsa_minimizer.minimize(block_func2,
+                                         tf.random.uniform(shape=[n]),
+                                         blocking=True,
+                                         allowed_increase=0.5,
+                                         max_iterations=it)
+        self.assertFalse(result.converged)
+        self.assertEqual(result.num_iterations, it)
+        self.assertEqual(result.objective_value, init * 3 * it + init)
+
+    def test_3_qubit_circuit(self):
+        """Test quantum circuit optimization, adapted from Qiskit SPSA testing
+        https://github.com/Qiskit/qiskit-terra/blob/main/test/python/algorithms/optimizers/test_spsa.py#L37
+        """
+        qubits = [cirq.GridQubit(0, i) for i in range(3)]
+        params = sympy.symbols("q0:9")
+        circuit = cirq.Circuit()
+        for i in qubits:
+            circuit += cirq.ry(np.pi / 4).on(i)
+        circuit += cirq.ry(params[0]).on(qubits[0])
+        circuit += cirq.ry(params[1]).on(qubits[1])
+        circuit += cirq.rz(params[2]).on(qubits[2])
+
+        circuit += cirq.CZ(qubits[0], qubits[1])
+        circuit += cirq.CZ(qubits[1], qubits[2])
+        circuit += cirq.rz(params[3]).on(qubits[0])
+        circuit += cirq.rz(params[4]).on(qubits[1])
+        circuit += cirq.rx(params[5]).on(qubits[2])
+
+        # Build the Keras model.
+        model = tf.keras.Sequential([
+            # The input is the data-circuit, encoded as a tf.string
+            tf.keras.layers.Input(shape=(), dtype=tf.string),
+            # The PQC layer returns the expected value of the
+            # readout gate, range [-1,1].
+            pqc.PQC(circuit,
+                    cirq.Z(qubits[0]) * cirq.Z(qubits[1]),
+                    repetitions=1024),
+        ])
+
+        initial_point = np.array([
+            0.82311034, 0.02611798, 0.21077064, 0.61842177, 0.09828447,
+            0.62013131
+        ])
+
+        result = spsa_minimizer.minimize(loss_function_with_model_parameters(
+            model, lambda x, y: x[0][0],
+            util.convert_to_tensor([cirq.Circuit()]), None),
+                                         initial_point,
+                                         max_iterations=100)
+
+        self.assertTrue(result.converged)
+        self.assertLess(result.objective_value.numpy(), -0.95)
 
     def test_keras_model_optimization(self):
         """Optimizate a PQC based keras model."""
@@ -157,16 +254,16 @@ class RotosolveMinimizerTest(tf.test.TestCase, parameterized.TestCase):
             tf.keras.layers.Input(shape=(), dtype=tf.string),
             # The PQC layer returns the expected value of the
             # readout gate, range [-1,1].
-            pqc.PQC(circuit, cirq.Z(q1)),
+            pqc.PQC(circuit, 2 * cirq.Z(q1)),
         ])
 
         # Initial guess of the parameter from random number
-        result = rotosolve_minimizer.minimize(
+        result = spsa_minimizer.minimize(
             loss_function_with_model_parameters(model, tf.keras.losses.Hinge(),
                                                 x_circ, y),
             tf.random.uniform(shape=[2]) * 2 * np.pi)
 
-        self.assertAlmostEqual(result.objective_value, 0)
+        self.assertAlmostEqual(result.objective_value.numpy(), 0)
         self.assertTrue(result.converged)
 
 
