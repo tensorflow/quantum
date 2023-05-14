d16aef9f016e8015315d1d838b590905f88a1b6d
Sinestro38 <pavanjayasinha@gmail.com>
2023-05-14 05:32:50 +0000

    add uncomment toggle

--- benchmarks/scripts/benchmark_cuquantum_ops.py
+++ benchmarks/scripts/benchmark_cuquantum_ops.py
@@ -27,9 +27,18 @@ _test_params_1 = BenchmarkParams(n_qubits=20, n_moments=1, batch_size=5)
 _test_params_2 = BenchmarkParams(n_qubits=21, n_moments=10, batch_size=5) # more depth
 _test_params_3 = BenchmarkParams(n_qubits=22, n_moments=1, batch_size=5, n_iters=10)
 
-TEST_PARAMS_EXPECTATION = [_test_params_1,]
-TEST_PARAMS_SAMPLED_EXPECTATION = [_test_params_1,]
-TEST_PARAMS_SAMPLES = [_test_params_1,]
+TEST_PARAMS_EXPECTATION = [
+    _test_params_1,
+    # _test_params_2, # uncomment for depth params
+    ]
+TEST_PARAMS_SAMPLED_EXPECTATION = [
+    _test_params_1,
+    # _test_params_2, # uncomment for depth params
+    ]
+TEST_PARAMS_SAMPLES = [
+    _test_params_1,
+    # _test_params_2, # uncomment for depth params
+    ]
 TEST_PARAMS_STATE = [_test_params_3,]
 
 def _measure_median_runtime(
