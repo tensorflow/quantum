7413a700499d14247047c64cc70948d9516cbd21
Michael Broughton <mbbrough@google.com>
2023-06-12 14:14:35 -0700

    Revert "Merge pull request #2 from jaeyoo/cuquantum_support"
    This reverts commit 46aa1f0eb43473967f81e14b00a706adefc37482, reversing
    changes made to 2a5fd073deecc9c9a07cf2d85e7befb3f3554fcb.

--- tensorflow_quantum/core/ops/tfq_simulate_ops_cuda_test.py
+++ tensorflow_quantum/core/ops/tfq_simulate_ops_cuda_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests that specifically target tfq_simulate_ops_cu*."""
+"""Tests that specifically target tfq_simulate_ops_cuda."""
 import os
 import time
 import numpy as np
@@ -22,7 +22,6 @@ import cirq
 
 from tensorflow_quantum.core.ops import tfq_simulate_ops
 from tensorflow_quantum.core.ops import tfq_simulate_ops_cuda
-from tensorflow_quantum.core.ops import tfq_simulate_ops_cuquantum
 from tensorflow_quantum.python import util
 
 def measure_average_runtime(fn, tag, num_samples=10):
@@ -37,7 +36,7 @@ def measure_average_runtime(fn, tag, num_samples=10):
     return avg_time, result
 
 
-class SimulateExpectationGpuTest(tf.test.TestCase):
+class SimulateExpectationTest(tf.test.TestCase):
     """Tests tfq_simulate_expectation."""
 
     def test_simulate_expectation_cpu_vs_cuda(self):
@@ -82,47 +81,6 @@ class SimulateExpectationGpuTest(tf.test.TestCase):
         # CUDA op should be faster than CPU op.
         self.assertGreater(cpu_avg_time, cuda_avg_time)
 
-    def test_simulate_expectation_cpu_vs_cuquantum(self):
-        """Make sure that cpu & gpu(cuquantum) ops have the same results."""
-        n_qubits = 20
-        batch_size = 5
-        symbol_names = ['alpha']
-        qubits = cirq.GridQubit.rect(1, n_qubits)
-        circuit_batch, resolver_batch = \
-            util.random_symbol_circuit_resolver_batch(
-                qubits, symbol_names, batch_size)
-
-        circuit_batch_tensor = util.convert_to_tensor(circuit_batch)
-
-        symbol_values_array = np.array(
-            [[resolver[symbol]
-              for symbol in symbol_names]
-             for resolver in resolver_batch])
-
-        pauli_sums = util.random_pauli_sums(qubits, 3, batch_size)
-        pauli_sums_tensor = util.convert_to_tensor([[x] for x in pauli_sums])
-
-        cpu_avg_time, res_cpu = measure_average_runtime(
-            lambda: tfq_simulate_ops.tfq_simulate_expectation(
-                circuit_batch_tensor,
-                symbol_names, symbol_values_array.astype(np.float64),
-                pauli_sums_tensor),
-            "CPU"
-        )
-
-        cuda_avg_time, res_cuda = measure_average_runtime(
-            lambda: tfq_simulate_ops_cuquantum.tfq_simulate_expectation(
-                circuit_batch_tensor,
-                symbol_names, symbol_values_array.astype(np.float64),
-                pauli_sums_tensor),
-            "cuQuantum"
-        )
-
-        # The result should be the similar within a tolerance.
-        np.testing.assert_allclose(res_cpu, res_cuda, atol=1e-5)
-
-        # cuQuantum op should be faster than CPU op.
-        self.assertGreater(cpu_avg_time, cuda_avg_time)
 
 if __name__ == "__main__":
     tf.test.main()
