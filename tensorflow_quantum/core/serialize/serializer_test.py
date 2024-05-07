bd90425788adb600ff37c664f4d9b6bb7d978d93
root <root@google.com>
2024-05-07 10:17:41 -0700

    Version bump again for wheel test.

--- tensorflow_quantum/core/serialize/serializer_test.py
+++ tensorflow_quantum/core/serialize/serializer_test.py
@@ -100,7 +100,7 @@ def _build_op_proto(gate_id, arg_names, arg_vals, qubit_ids):
         if isinstance(arg_vals[i], str) else \
             program_pb2.Arg(
                 arg_value=program_pb2.ArgValue(
-                    float_value=np.round(float(arg_vals[i]), 6))))     \
+                    float_value=np.round(float(arg_vals[i]), 6))))      \
                 for i in range(len(arg_vals))},
         qubits=[program_pb2.Qubit(
             id=q_id) for q_id in qubit_ids])])
