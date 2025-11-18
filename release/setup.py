1377ee1a72f88d3ed221626d06ecab9e57558b5f
psamanoelton <psamano@griddynamics.com>
2025-11-18 11:09:35 -0600

    Update release/setup.py
    Co-authored-by: Michael Hucka <mhucka@google.com>

--- release/setup.py
+++ release/setup.py
@@ -102,6 +102,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",                
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Scientific/Engineering :: Mathematics",
