8dc0e2b0bffe11c39ceccd5379b17193534db84b
mhucka <mhucka@google.com>
2025-12-06 00:48:19 +0000

    Simplify run_auditwheel.sh
    The script in third_party/tf/auditwheel does not seem necessary (and
    TensorFlow 2.16 doesn’t even have such a script anymore). As far as the
    munging of the policy file that third_party/tf/auditwheel was doing,
    adding `--exclude libtensorflow_framework.so.2` to auditwheel appears to
    be enough to solve the problem. (At least, the resulting wheel has
    worked in testing on Colab.)

--- release/run_auditwheel.sh
+++ release/run_auditwheel.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,64 +19,53 @@
 set -eu
 
 IMAGE_TAG="quay.io/pypa/manylinux_2_34_x86_64"
-AUDITWHEEL="third_party/tf/auditwheel"
 
 # Default values for variables that can be changed via command line flags.
 py_version="3.11"
+action="repair"
 verbose=""
 
-usage="Usage: ${0} [OPTIONS] <command> <file>
-Runs auditwheel <command> on the wheel in <file>.
-<command> must be one of 'show' or 'repair'.
-Available options:
+usage="Usage: ${0} [OPTIONS] /path/to/wheel
+Run auditwheel on the given wheel file. Available options:
   -h  Show this help message and exit
-  -p  Python version (default: ${py_version})
+  -p  Python version to use (default: ${py_version})
+  -s  Run auditwheel show, not repair (default: run auditwheel repair)
   -v  Verbose output"
 
-while getopts "hp:v" opt; do
+while getopts "p:hsv" opt; do
     case "${opt}" in
         p) py_version="${OPTARG}" ;;
+        s) action="show" ;;
         v) verbose="--verbose" ;;
         h) echo "${usage}"; exit 0 ;;
         *) echo "${usage}" >&2; exit 1 ;;
     esac
 done
 shift $((OPTIND -1))
-if [ $# -ne 2 ]; then
+if [ ! $# -ge 1 ]; then
     echo "ERROR: insufficient arguments."
     echo "${usage}" >&2
     exit 1
 fi
 
-action="$1"
-if [ "${action}" != "show" ] && [ "${action}" != "repair" ]; then
-    echo "ERROR: Command must be 'show' or 'repair'." >&2
-    exit 1
-fi
-
-wheel_path="$(realpath "${2}")"
-wheel_name="$(basename "${2}")"
+wheel_path="$(realpath "${1}")"
+wheel_name="$(basename "${1}")"
 
 # Change to the top of the local TFQ git tree.
 thisdir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd -P)
 repo_dir=$(git -C "${thisdir}" rev-parse --show-toplevel) || exit $?
 cd "${repo_dir}" || exit $?
 
-mkdir -p "${repo_dir}/wheelhouse"
-
-if [ "${action}" == "show" ]; then
-    cmd="${verbose} show"
-else
-    cmd="${verbose} repair --wheel-dir ${repo_dir}/wheelhouse"
+args="${action} ${verbose} --plat manylinux_2_34_x86_64"
+if [ "${action}" = "repair" ]; then
+    args="${args} --exclude libtensorflow_framework.so.2 -w /tfq/wheelhouse"
 fi
 
-docker run -i --rm --network host \
+docker run -it --rm --network host \
     -w /tfq \
     -v "${PWD}":/tfq \
     -v "${wheel_path}":"/tmp/${wheel_name}" \
-    -e py_version="${py_version}" \
-    -e IMAGE_TAG="${IMAGE_TAG}" \
     "${IMAGE_TAG}" \
-    bash -c "/tfq/${AUDITWHEEL} '${cmd}' '/tmp/${wheel_name}'"
+    bash -c "auditwheel ${args} /tmp/${wheel_name}"
 
-echo "Done."
+echo "Done. New wheel file written to ${repo_dir}/wheelhouse"
