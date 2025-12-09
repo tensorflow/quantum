95d4f39fc7521da41c329fa388cb35e0ead54287
mhucka <mhucka@google.com>
2025-12-09 03:14:46 +0000

    Add script for generating requirements.txt

---
+++ scripts/generate_requirements.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Copyright 2025 The TensorFlow Quantum Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+# Summary: regenerate requirements.txt using pip-compile & munging the result.
+
+set -eu
+
+# Find the top of the local TFQ git tree. Do it early in case this fails.
+thisdir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd -P)
+repo_dir=$(git -C "${thisdir}" rev-parse --show-toplevel 2>/dev/null || \
+  echo "${thisdir}/..")
+
+echo "Running pip-compile …"
+pip-compile -q --no-strip-extras --allow-unsafe
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  # macOS's default sed handles -i option differently.
+  sed_arg="-i ''"
+else
+  sed_arg="-i"
+fi
+
+echo "Adjusting output of pip-compile …"
+sed "${sed_arg}" -e '/^--index-url/d' requirements.txt
+sed "${sed_arg}" -e '/^--extra-index-url/d' requirements.txt
+sed "${sed_arg}" -e 's/^pyyaml==.*/pyyaml/' requirements.txt
+
+echo "Done."
