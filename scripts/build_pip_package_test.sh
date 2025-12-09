a2ebde66108013c684f8c7100680fdb34e19bc2a
mhucka <mhucka@google.com>
2025-12-09 20:47:14 +0000

    Revert more formatting changes I did in a past commit

--- scripts/build_pip_package_test.sh
+++ scripts/build_pip_package_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Copyright 2020 The TensorFlow Quantum Authors. All Rights Reserved.
-#
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +17,7 @@
 pip install -r requirements.txt
 
 # cd tensorflow_quantum
-printf "y\n" | ./configure.sh
+echo "Y\n" | ./configure.sh
 
 bazel build -c opt --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=1" --cxxopt="-msse2" --cxxopt="-msse3" --cxxopt="-msse4" release:build_pip_package
 rm /tmp/tensorflow_quantum/* || echo ok
