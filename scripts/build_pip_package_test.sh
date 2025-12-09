f9df640da30488a4373df45e7d0b84d427cba718
mhucka <mhucka@google.com>
2025-12-09 06:01:13 +0000

    Move a compilation flags change to a separate PR
    These flags are redundant but also not something that necessarily has to
    removed as part of this PR.

--- scripts/build_pip_package_test.sh
+++ scripts/build_pip_package_test.sh
@@ -19,7 +19,7 @@ pip install -r requirements.txt
 # cd tensorflow_quantum
 printf "y\n" | ./configure.sh
 
-bazel build -c opt --cxxopt="-msse2" --cxxopt="-msse3" --cxxopt="-msse4" release:build_pip_package
+bazel build -c opt --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=1" --cxxopt="-msse2" --cxxopt="-msse3" --cxxopt="-msse4" release:build_pip_package
 rm /tmp/tensorflow_quantum/* || echo ok
 bazel-bin/release/build_pip_package /tmp/tensorflow_quantum/
 pip install -U /tmp/tensorflow_quantum/*.whl
