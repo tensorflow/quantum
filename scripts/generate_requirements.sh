6b8f41c47636258b7929b44720026631c7322f9c
mhucka <mhucka@google.com>
2025-12-11 19:55:37 +0000

    Add support for a requirements constraint file
    Some of the TFQ transitive dependencies have recently gotten Dependabot
    alerts to be updated for some security flaws. However running
    pip-compile again does not end up updating those dependencies. It seems
    that the recommended way to handle this is to use a constraint file.
    
    This updates `./scripts/generate_requirements.sh` to invoke pip-compile
    with a constraint file.
    
    It also tells pip-compile to write the name of this script into the
    header of the requirements.txt file it generates.

--- scripts/generate_requirements.sh
+++ scripts/generate_requirements.sh
@@ -20,12 +20,42 @@
 set -eu
 
 # Find the top of the local TFQ git tree. Do it early in case this fails.
-thisdir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd -P)
+thisdir=$(CDPATH="" cd -- "$(dirname -- "${0}")" && pwd -P)
 repo_dir=$(git -C "${thisdir}" rev-parse --show-toplevel 2>/dev/null || \
   echo "${thisdir}/..")
 
+usage="Usage: ${0}
+Generate TFQ requirements.txt file from requirements.in."
+
+# Exit early if the user requested help.
+if [[ "${1:-}" == "-h" || "${1:-}" == "--help" || "${1:-}" == "help" ]]; then
+    echo "$usage"
+    exit 0
+fi
+
+# Ensure we have pip-compile.
+if ! pip show -qq pip-tools; then
+    echo "Python pip-tools not found. Please run 'pip install pip-tools'."
+    exit 1
+fi
+
+# Tell pip-compile to put this command in the comment header in requirements.txt
+export CUSTOM_COMPILE_COMMAND="${0}"
+
+constraint=""
+constraint_file="${repo_dir}/requirements-pins.txt"
+if [[ -e "${constraint_file}" ]]; then
+    constraint="--constraint $(realpath --relative-to=. "${constraint_file}")"
+fi
+
 echo "Running pip-compile in ${repo_dir} …"
-pip-compile -q --no-strip-extras --allow-unsafe
+# shellcheck disable=SC2086
+pip-compile -q \
+    --rebuild \
+    --allow-unsafe \
+    --no-strip-extras \
+    --no-emit-index-url \
+     ${constraint}
 
 declare -a inplace_edit=(-i)
 if [[ "$(uname -s)" == "Darwin" ]]; then
@@ -33,10 +63,12 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
   inplace_edit+=('')
 fi
 
+# Pyyaml is a transitive dependency, and pinning the version (as pip-compile
+# does) leads to unsatisfiable constraints on some platforms. However, we
+# don't need pyyaml to be a particular version. There's no easy way to tell
+# pip-compile not to constrain a particular package, so
 echo "Adjusting output of pip-compile …"
 sed "${inplace_edit[@]}" \
-  -e '/^--index-url/d' \
-  -e '/^--extra-index-url/d' \
   -e 's/^pyyaml==.*/pyyaml/' \
   requirements.txt
 
