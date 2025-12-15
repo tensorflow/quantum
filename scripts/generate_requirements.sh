7f248ea0a3c1878f907ac41ac4b30f99abb5220d
mhucka <mhucka@google.com>
2025-12-15 05:45:12 +0000

    Add pins file handling to generate_requirements & simplify
    A shortcoming of generating requirements.txt with version pins is that
    the versions of some transitive dependencies may need to be updated to
    address security releases. Simply re-running pip-compile to produce a
    new requirements.txt may not update the versions because the
    dependencies that bring in the transitive dependencies have not
    themselves changed. The recommended practice for this situation is to
    put the version constraints into a separate constraint file and pass it
    as an argument to pip-compile. (pip-compile has explicit support for
    this.) Some additions to the script scripts/generate_requirements.sh
    encapsulate and document this process.
    
    In addition, I removed some no-longer-needed post-processing of the
    requirements.txt file. It was a hack in the first place, and with the
    latest requirements.in, it turns out to be unnecessary.
    
    Finally, I simplified this some more and made it follow more style
    guidelines for Bash scripts.

--- scripts/generate_requirements.sh
+++ scripts/generate_requirements.sh
@@ -15,29 +15,36 @@
 # ==============================================================================
 
 # Summary: produce requirements.txt using pip-compile & munging the result.
-# Usage: ./generate_requirements.sh
+# Usage: ./scripts/generate_requirements.sh from the top dir of the repo.
 
 set -eu
 
 # Find the top of the local TFQ git tree. Do it early in case this fails.
-thisdir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd -P)
+thisdir=$(CDPATH="" cd -- "$(dirname -- "${0}")" && pwd -P)
 repo_dir=$(git -C "${thisdir}" rev-parse --show-toplevel 2>/dev/null || \
   echo "${thisdir}/..")
 
-echo "Running pip-compile in ${repo_dir} …"
-pip-compile -q --no-strip-extras --allow-unsafe
-
-declare -a inplace_edit=(-i)
-if [[ "$(uname -s)" == "Darwin" ]]; then
-  # macOS uses BSD sed, which requires a suffix for -i.
-  inplace_edit+=('')
+# Ensure we have pip-compile.
+if ! pip show -qq pip-tools; then
+    echo "Python pip-tools not found. Please run 'pip install pip-tools'."
+    exit 1
 fi
 
-echo "Adjusting output of pip-compile …"
-sed "${inplace_edit[@]}" \
-  -e '/^--index-url/d' \
-  -e '/^--extra-index-url/d' \
-  -e 's/^pyyaml==.*/pyyaml/' \
-  requirements.txt
+declare -a constraint=()
+pins_file="$(realpath --relative-to=. "${repo_dir}/requirements-pins.txt")"
+if [[ -e "${pins_file}" ]]; then
+    constraint+=(--constraint "${pins_file}")
+fi
+
+# Tell pip-compile to reference this script in the requirements.txt comments.
+export CUSTOM_COMPILE_COMMAND="${0}"
+
+echo "Running pip-compile in ${repo_dir} …"
+pip-compile -q \
+    --rebuild \
+    --allow-unsafe \
+    --no-strip-extras \
+    --no-emit-index-url \
+     "${constraint[@]}"
 
 echo "Done."
