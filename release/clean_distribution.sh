0fb88fb0fe64f1abdfea17e1889cc1e0a15bccea
mhucka <mhucka@google.com>
2025-12-24 02:59:59 +0000

    Fix license headers

--- release/clean_distribution.sh
+++ release/clean_distribution.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Copyright 2025 Google LLC
+# Copyright 2025 The TensorFlow Quantum Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# https://www.apache.org/licenses/LICENSE-2.0
+#     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
