03d02ce490a6febf476f4be1b03ff80acd5f88a5
mhucka <mhucka@google.com>
2026-05-12 04:48:55 +0000

    Fix linter errors

--- scripts/format_ipynb.py
+++ scripts/format_ipynb.py
@@ -18,24 +18,6 @@ import glob
 import nbformat
 import yapf
 
-# Must be run from the top level of the `TFQuantum` repo.
-NOTEBOOKS = glob.glob("docs/tutorials/*.ipynb")
-for fname in NOTEBOOKS:
-    nb = nbformat.read(fname, as_version=nbformat.NO_CONVERT)
-    all_cells = nb.get('cells')
-    for i, cell in enumerate(all_cells):
-        if cell.get('cell_type') != 'code':
-            continue
-        lines = cell.get('source')
-        # This will safely skip over cells containing !% magic
-        try:
-            fmt_lines = yapf.yapf_api.FormatCode(''.join(lines),
-                                                 filename=fname)[0]
-        except (SyntaxError, yapf.yapflib.errors.YapfError):
-            continue
-        # google style always adds an EOF newline; undo this.
-        all_cells[i]['source'] = fmt_lines[:-1]
-
 
 def format_notebooks():
     """Format tutorial notebooks.
